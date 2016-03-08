0d02d2fbc7f4d92a30660a764f491ed6ede4a943
Chris Li <chrisli@hotmail.ca>
2016-03-07 18:06:39 -0800

    - Added web assets - Changed front-end to use gorilla/websocket in order to assign connecting clients with a replica's RPC address (to be used for client-to-replica interoperability)
    NOTE:
    - use "http://localhost:8080" in the browser to see web page contents

--- front-end.go
+++ front-end.go
@@ -4,13 +4,12 @@ import (
 	"fmt"
 	"os"
 	"net"
-	"net/rpc"
 	"encoding/json"
-	"time"
 	"flag"
 	"net/http"
 	"text/template"
 	"log"
+	"github.com/gorilla/websocket"
 )
 
 type Replica struct {
@@ -21,24 +20,23 @@ type Replica struct {
 // map to where the key is the replica id and the value is the replica's IP
 var replicaMap map[string]string
 
-var httpAddress = flag.String("addr", ":8080", "http service address")
 var homeTemplate = template.Must(template.ParseFiles("home.html"))
+var upgrader = websocket.Upgrader{} // use default options
+var httpAddress = flag.String("addr", ":8080", "http service address")
 
 // Main server loop.
 func main() {
 	// Parse args.
 	usage := fmt.Sprintf("Usage: %s [client ip:port] [replica-node ip:port]\n",
 		os.Args[0])
-	if len(os.Args) != 3 {
+	if len(os.Args) != 2 {
 		fmt.Printf(usage)
 		os.Exit(1)
 	}
 	
 	replicaMap = make(map[string]string)
 
-	clientAddrString := os.Args[1]
-	
-	replicaAddrString := os.Args[2]
+	replicaAddrString := os.Args[1]
 	replicaAddr, err := net.ResolveUDPAddr("udp", replicaAddrString)
 	checkError(err)
 	
@@ -50,33 +48,50 @@ func main() {
 	}
 	
 	go ReplicaListener(replicaConn)
-	// go ReplicaActivityListener()
-	
-	// start TCP server to listen for new clients
-	clientListener, err := net.Listen("tcp", clientAddrString)
-	if err != nil {
-		fmt.Println("Error on TCP listen: ", err)
-		os.Exit(-1)
-	}
 	
 	flag.Parse()
 	// start HTTP server
-	go func() {
-		http.HandleFunc("/", ServeHome)
-		err = http.ListenAndServe(*httpAddress, nil)
-		if err != nil {
-			log.Fatal("Error on HTTP serve: ", err)
-		}
-	}()
-	
-	for {
-		clientConn, err := clientListener.Accept()
-		checkError(err)
-		go RouteClient(clientConn)		
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./"))))
+	http.HandleFunc("/route", RouteClient)
+	http.HandleFunc("/home", ServeHome)
+	err = http.ListenAndServe(*httpAddress, nil)
+	if err != nil {
+		log.Fatal("Error on HTTP serve: ", err)
 	}
 }
 
-// serve the home page at localhost:8080
+// Send a replica's RPCAddress to client to start communication with it
+func RouteClient(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, _, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		
+		// naively get the first replica's address 
+		// TODO: Add logic to test availability of replica and remove the replica from map
+		var addressToSend string
+		for _, RPCAddress := range replicaMap {
+			addressToSend = RPCAddress
+			break
+		}
+		
+		err = c.WriteMessage(mt, []byte(addressToSend))
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+// Serve the home page at localhost:8080
 func ServeHome(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.Error(w, "Not found", 404)
@@ -87,37 +102,10 @@ func ServeHome(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	homeTemplate.Execute(w, r.Host)
+	homeTemplate.Execute(w, "ws://" + r.Host + "/route")
 }
 
-// Send a replica's RPCAddress to client to start communication with it
-func RouteClient(conn net.Conn) {
-
-	for _, RPCAddress := range replicaMap {
-		conn.Write([]byte(RPCAddress))
-		conn.Close()	
-		break
-	}
-}
-
-// Periodically check each node for availability (every second)
-func ReplicaActivityListener() {
-
-	for {
-		for nodeId, RPCaddress := range replicaMap {
-			_, err := rpc.Dial("tcp", RPCaddress)
-			if err != nil {
-				// remove the replica in the map
-				fmt.Println("Removed replica: " + nodeId)
-				delete(replicaMap, nodeId)
-			}
-		}
-		
-		time.Sleep(1000 * time.Millisecond)
-	}
-}
-
-// Function to listen for newly connected replica nodes
+// Listen for newly connected replica nodes
 func ReplicaListener(conn *net.UDPConn) {
 	
 	buf := make([]byte, 1024)
---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	
+	var websocket = new WebSocket("ws://localhost:8080/route");	
+	websocket.onopen = function(event) {
+		console.log("opened");
+		// Ping the websocket
+		websocket.send("test")
+	}
+	websocket.onclose = function(event) {
+		console.log("closed");
+	}
+	websocket.onmessage = function(event) {
+		$("#rpc_address").text("Assigned replica RPC Address: " + event.data)
+	}
+	
+});
