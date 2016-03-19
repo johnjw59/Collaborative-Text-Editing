ad5a6f0e427959cf4d3207f220546506f79db9c1
John Wiebe <johnjw59@gmail.com>
2016-03-19 02:35:43 -0700

    copied over websocket code

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
 angular.module('app', ['angular-jsonrpc-client', 'eb.caret'])
 .controller('main', function($scope, jsonrpc) {
-  
+  var ws = new WebSocket('ws://localhost:8080/route');
+  ws.onopen = function() {
+    ws.send('ping');
+  };
+  ws.onclose = function() {
+    console.log('websocket closed');
+  };
+  ws.onmessage = function(event) {
+    $scope.replica_ip = event.data;
+    $scope.$apply();
+  };
+
+
 });
